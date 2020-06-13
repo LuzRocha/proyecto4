e44c6933e7956958cfeef62495d1c2720e713e68
Luz Rosales <luzroche199820@gmail.com>
2020-06-12 23:15:58 -0500

    mi proyecto4

---
+++ PROMISES/app.js
@@ -0,0 +1,17 @@
+// Promises
+
+const aplicarDescuento = new Promise(function(resolve, reject) {
+    const descuento = false;
+    if(descuento) {
+        resolve('Descuento Aplicado');
+      } else {
+        reject('No se puede aplicar el descuento');
+
+      }
+});
+    aplicarDescuento.then(function(resultado) {
+        console.log(resultado);
+}).catch(function(error){
+        console.log(error);
+});
+
